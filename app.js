6000320528342a73f9fdd3ac08959d5b5ab3a4d8
athenaozanich <athenaozanich@gmail.com>
2020-03-06 05:21:20 -0500

    update files and error template

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set('view engine', 'pug')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.locals.fewdProjects = data.fewdProjects
+  res.locals.fewdProjects = data.projects
   res.locals.fsjsProjects = data.fsjsProjects
   res.render('index', {})
 })
@@ -17,14 +17,13 @@ app.get('/about', (req, res) => {
 })
 
 app.get('/project/:devType/:project', (req, res, next) => {
-  const type = `${req.params.devType}Projects`
+  const type = 'projects'
   if (data[type][req.params.project - 1]) {
     res.locals.project = data[type][req.params.project - 1]
     res.render('project', {})
   } else {
     const err = new Error('Oh Noes! Seems that project does not exist... YET!')
     err.status = 404
-    console.log(err.stack)
     next(err)
   }
 })
@@ -38,7 +37,8 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   res.locals.error = err
   res.status(err.status)
-  res.render('err')
+  console.log(err.message, err.status)
+  res.render('error')
 })
 app.listen(3000, () => {
   console.log('The server is finished proccessing the request and loading the response on localhost:3000')
